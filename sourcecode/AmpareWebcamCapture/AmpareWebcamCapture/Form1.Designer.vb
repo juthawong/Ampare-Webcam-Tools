616ad464a282fec9d5136004107cb060f5fb89e9
Juthawong Naisanguansee <juthawong@gmail.com>
2015-07-05 09:32:53 +0700

    Publish Sourcecode
    Publish a new source code . Hope you like it

---
+++ sourcecode/AmpareWebcamCapture/AmpareWebcamCapture/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 161)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(541, 100)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Tools"
+        '
+        'Button3
+        '
+        Me.Button3.BackgroundImage = Global.AmpareWebcamCapture.My.Resources.Resources.Stop_Normal_Red_icon
+        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button3.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Button3.Location = New System.Drawing.Point(78, 16)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(86, 81)
+        Me.Button3.TabIndex = 3
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.BackgroundImage = Global.AmpareWebcamCapture.My.Resources.Resources.Image_Capture_icon
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button2.Location = New System.Drawing.Point(463, 16)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 81)
+        Me.Button2.TabIndex = 2
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImage = Global.AmpareWebcamCapture.My.Resources.Resources.Actions_media_record_icon
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Button1.Location = New System.Drawing.Point(3, 16)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 81)
+        Me.Button1.TabIndex = 1
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(541, 161)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.FileName = "defa.jpg"
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(541, 261)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Ampare Webcam Tools"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ sourcecode/AmpareWebcamCapture/AmpareWebcamCapture/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports Emgu.CV
+Imports Emgu.CV.Structure
+Imports Emgu.Util
+Public Class Form1
+    Dim Capturez As Capture = New Capture
+    Dim Videoz As VideoWriter
+    Dim imagez As Image(Of Bgr, Byte)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button3.Visible = False
+
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        imagez = Capturez.RetrieveBgrFrame
+        Videoz.WriteFrame(imagez)
+        PictureBox1.Image = imagez.ToBitmap()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+
+     
+        'CV_FOURCC("P","I&quo­t;,"M","1") = MPEG-1
+        'CV_FOURCC("M","J&quo­t;,"P","G") = motion-jpeg
+        'CV_FOURCC("M", "P", "4", "2") = MPEG-4.2
+        'CV_FOURCC("D", "I", "V", "3") = MPEG-4.3
+        'CV_FOURCC("D", "I", "V", "X") = MPEG - 4
+        'CV_FOURCC("U", "2", "6", "3") = H263
+        'CV_FOURCC("I", "2", "6", "3") = H263I
+        'CV_FOURCC("F", "L", "V", "1") = FLV1
+        SaveFileDialog1.Title = "Save Video as"
+        SaveFileDialog1.DefaultExt = "MP4"
+        SaveFileDialog1.Filter = "MPEG-4 Video Files (*.mp4)|*.mp4"
+
+        Dim Destination As String = "D:\defa.mpg"
+        Dim compression As Integer = CvInvoke.CV_FOURCC("D", "I", "V", "x")
+
+        Dim fps As Integer = 5
+        Dim width As Integer = PictureBox1.Width
+        Dim Height As Integer = PictureBox1.Height
+        Dim color As Boolean = True
+
+        Videoz = New VideoWriter(Destination, compression, fps, width, Height, color)
+        Button1.Visible = False
+        Button3.Visible = True
+
+        Timer2.Stop()
+
+        Timer1.Start()
+
+
+        Catch ex As Exception
+            MsgBox("Ops, Something Wrong : Error Code : " + ex.Message)
+            Exit Sub
+        End Try
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        imagez = Capturez.RetrieveBgrFrame
+        PictureBox1.Image = imagez.ToBitmap()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SaveFileDialog1.Title = "Save Image as"
+        SaveFileDialog1.DefaultExt = "JPG"
+        SaveFileDialog1.Filter = "JPEG Image Files (*.jpg)|*.jpg"
+        Dim Destination As String
+        If (SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK) Then
+            Destination = SaveFileDialog1.FileName
+        Else
+            Exit Sub
+        End If
+        PictureBox1.Image.Save(Destination, System.Drawing.Imaging.ImageFormat.Jpeg)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Timer1.Stop()
+        Timer2.Start()
+        Button1.Visible = True
+        Button3.Visible = False
+    End Sub
+
+
+End Class
